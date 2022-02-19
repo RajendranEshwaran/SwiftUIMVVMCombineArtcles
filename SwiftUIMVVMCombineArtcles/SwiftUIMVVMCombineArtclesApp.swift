190f6a907363de1e5a03cec4222ca8f729a8a97f
RajendranEshwaran <rajendranmca2010@gmail.com>
2022-02-19 10:36:56 -0500

    Initial Commit

---
+++ SwiftUIMVVMCombineArtcles/SwiftUIMVVMCombineArtclesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIMVVMCombineArtclesApp.swift
+//  SwiftUIMVVMCombineArtcles
+//
+//  Created by gomathi saminathan on 2/19/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIMVVMCombineArtclesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
